684ec9c232c6fef6eb2ff5200b10c78df366bffb
Sweeks <john.suihkonen@gmail.com>
2016-07-10 14:15:13 -0500

    init commit

---
+++ server/server.js
@@ -0,0 +1,30 @@
+//server.js
+var firebase = require("firebase");
+var serverCmds = require("./serverCmds.js")
+
+var mainApp;
+var key = "AIzaSyDfeAhkESQtU8WcaNME-T3AuOM3IggMxuY";
+var auth = "pogomaps-c5016.firebaseapp.com";
+var URL = "https://pogomaps-c5016.firebaseio.com";
+var storage = "pogomaps-c5016.appspot.com"
+
+var config = {
+	apiKey: key,
+	authDomain: auth,
+	databaseURL: URL,
+	storageBucket: storage,
+};
+
+mainApp = firebase.initializeApp(config);
+
+var ref = mainApp.database().ref("listenCoords");
+ref.on('child_added', function(childSnapshot, prevChildKey) {
+	serverCmds.postDate(childSnapshot, mainApp.database());
+	serverCmds.postType(childSnapshot, mainApp.database());
+	serverCmds.postMap(childSnapshot, mainApp.database());
+	serverCmds.postMon(childSnapshot, mainApp.database());
+	//console.log(snapValue);
+	var key = childSnapshot.getKey();
+	mainApp.database().ref("listenCoords/" + key).remove();
+	console.log("complete");
+});
---
+++ server/serverCmds.js
@@ -0,0 +1,72 @@
+//serverCmds.js
+var firebase = require("firebase");
+var pokedex = require("./pokemonData.json");
+
+exports.postMap = function(snapShot, app) {
+	var snapVal = snapShot.val();
+	var city = snapVal["city"];
+	var lat = snapVal["lat"];
+	var lon = snapVal["long"];
+	var name = snapVal["name"];
+	var id = pokedex[name]["id"];
+	var updates = {
+		id: id,
+		lat: lat,
+		long: lon
+	};
+	app.ref("mapCoords/" + city).push(updates);
+}
+
+exports.postType = function(snapShot, app) {
+	var snapVal = snapShot.val();
+	var lat = snapVal["lat"];
+	var lon = snapVal["long"];
+	var name = snapVal["name"];
+	var id = pokedex[name]["id"];
+	var type = pokedex[name]["type"];
+	var updates = {
+		id: id,
+		lat: lat,
+		long: lon
+	};
+	type.forEach(function(item, index) {
+		app.ref("type/" + item).push(updates);
+	});
+}
+
+exports.postMon = function(snapShot, app) {
+	var snapVal = snapShot.val();
+	var lat = snapVal["lat"];
+	var lon = snapVal["long"];
+	var name = snapVal["name"];
+	var id = pokedex[name]["id"];
+	var updates = {
+		lat: lat,
+		long: lon
+	};
+	app.ref("pokemon/" + id + "/catches").push(updates);
+	app.ref("pokemon/" + id + "/name").once("value").then (function(snap) {
+		var a = snap.exists();
+		if (!a) {
+			app.ref("pokemon/" + id + "/name").set(name);
+			app.ref("pokemon/" + id + "/gen").set(pokedex[name]["gen"]);
+			app.ref("pokemon/" + id + "/type").set(pokedex[name]["type"]);
+		}
+	});
+}
+
+exports.postDate = function(snapShot, app) {
+	var snapVal = snapShot.val();
+	var mon = snapVal["month"];
+	var day = snapVal["day"];
+	var lat = snapVal["lat"];
+	var lon = snapVal["long"];
+	var name = snapVal["name"];
+	var id = pokedex[name]["id"];
+	var updates = {
+		id: id,
+		lat: lat,
+		long: lon
+	};
+	app.ref("date/" + mon + "/" + day).push(updates);
+}
